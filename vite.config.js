8a4f9637178cf95d3f842b517b97d0542183a436
曾阳 <209337357+ZENG-03@users.noreply.github.com>
2026-04-09 09:50:07 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/text-adventure-game/', // 必须和你的 GitHub 仓库名称一致！否则会报 404
   plugins: [vue()],
   server: {
     port: 3000
